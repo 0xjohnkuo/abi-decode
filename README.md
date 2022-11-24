534fa6e0e0cdb4778c9b95767d1cd84e9d1ab523
0xjohnkuo <john.kuo@crypto.com>
2022-11-24 17:19:12 +0800

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # abi-decode
+
+## installation
+
+1. copy repository
+> git clone https://github.com/0xjohnkuo/abi-decode
+
+2. install package at local
+> cd abi-decode && npm install
+
+3. edit input.yaml, and paste in the contract abi
+contract abi can be found at cronoscan
+for example, the `Contract ABI` section of https://cronoscan.com/address/0x8d13982c702fe7c6537529986df67dabeafc4c19#code
+
+4. start decoding
+> npm start
+
+5. result example
+```
+input#0: _toTokenOrLp, type: address
+input#1: _outputAmountMin, type: uint256
+--- result ---
+input#0: 0xbf62c67eA509E86F07c8c69d0286C0636C50270b
+input#1: 63454539553771227830
+```
---
+++ main.js
@@ -0,0 +1,27 @@
+const { ethers } = require("ethers");
+const readYamlFile = require('read-yaml-file')
+
+readYamlFile('input.yaml').then(data => {
+    let found = false
+    console.log(`function name: ${data.function}`)
+    data.abi.forEach(val=>{
+        if (val.name == data.function && val.type == "function") {
+            val.inputs.forEach((input, idx) =>{
+                console.log(`input#${idx}: ${input.name}, type: ${input.type}`)
+
+            })
+            found = true
+        }
+    })
+    if (!found){
+        console.error(`cannot find ${data.function} in the provided abi`)
+        return
+    }
+    console.log("--- result ---")
+    const iface = new ethers.utils.Interface(data.abi)
+    let result = iface.decodeFunctionData(data.function, data.input)
+    // console.log(data.abi[])
+    result.forEach((val, idx)=>{
+        console.log(`input#${idx}: ${val}`)
+    })
+})
